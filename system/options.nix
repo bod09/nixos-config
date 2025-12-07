2d341a40010d69f86cc18410071967f105af6c22
bod09 <bodps3@hotmail.co.uk>
2025-12-07 23:47:08 +0000

    Switched to iwd for wireless

--- system/options.nix
+++ system/options.nix
@@ -20,17 +20,28 @@ in
   hardware.graphics = {
   enable = true;
   enable32Bit = true;
-};
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.iwd.settings = {
+    IPv6 = {
+      Enabled = true;
+    };
+    Settings = {
+      AutoConnect = true;
+    };
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/London";
